85857bf121d3845b0a562b902533656de498f2f8
Paul Borbiro <paulborbiro@gmail.com>
2024-02-29 20:32:53 +0200

    expenses-app

---
+++ expenses.frontend/src/services/expenses.js
@@ -0,0 +1,12 @@
+export const GetExpenses = async (dispatch) => {
+    try {
+// API call
+        const expenses = [
+            {id: 1, descriptions: 'Groceries', ammount: 23.15},
+            {id: 2, descriptions: 'Gas', ammount: 123.15},
+            {id: 3, descriptions: 'Loans', ammount: 1222.15},
+        ]
+    } catch {
+        console.log('Error!')
+    }
+}
